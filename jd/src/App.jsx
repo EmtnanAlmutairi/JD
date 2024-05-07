b91e306e7c370d812d650e8fd24659ffd2c6983e
امتنان المطيري <imeland925@gmail.com>
2024-05-07 22:11:37 +0300

    imgs

--- jd/src/App.jsx
+++ jd/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './component/navbar'; // Import your Navbar component
 import Hero from './component/hero'; // Import your Hero component
 import Blog from './component/blog';
+import Expert from './component/expert';
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <Blog/>
+      <Expert/>
     </div>
   );
 }
